bd8452f1f6cd1e6fd1f27b30a962bb20483b3717
Anas Aziach <anasaziach@gmail.com>
2022-03-04 15:57:28 +0100

    home_page v1

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="home_page.js"></script>
+    <title>test</title>
+</head>
+<body>
+    <!--bar d'enhaut pour Desktop-->
+    <div class="headerr container-fluid" id="testt">
+        <div class="headerr2 row">
+            <div class="head col-md-6 col-lg-7 col-xl-8">
+                <p>you want to work with us?</p>
+                <h5>Become a Volunter</h5>
+                <p style="padding-left:20%;font-size:16px;font-weight:bold">+212 707184109</p>
+            </div>
+            <div class="col-md-5 col-lg-4 col-xl-3">
+                <div class="head2">
+                    <a href="#"><h4>DONATION</h4></a>
+                </div>
+            </div>
+            <div class="head3 col-md-1 col-lg-1 col-xl-1">
+                <a href="#">I'm a member</a>
+            </div>
+        </div>
+    </div>
+    <!--barre d'en haut pour Desktop-->
+    <div class="container-fluid px-0 bar" id="barre_test">
+        <div class="barr row ">
+            <div class="col-md-2 col-lg-5 col-xl-6">
+                <h2 class="logo" id="mylogo">Souffle<span class="test">.</span></h2>
+            </div>
+            <div class=" navbar navbar-expand-sm col-lg-7 col-md-10 col-xl-6">
+                <ul class="bar1 navbar-nav" id="items-links">
+                    <li class="nav-item">
+                        <a class="nav-link link" href="#home">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link" href="#about">ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link" href="#section3">BLOG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link" href="#section1">PROJECTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link" href="#section2">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--bar d'enhaut pour mobile-->
+    <div class="sec2 container-fluid px-0">
+      <nav class="navbar navbar-expand-md bg-light">
+        <h2 class="logoo">Souffle<span class="test">.</span></h2>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="border:1px solid gray">
+            <img src="ressources/menu.png" width="30px" height= "30px">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link " href="#">HOME</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">ABOUT</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">CONTACT</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">DONATE</a></li>
+          </ul>
+              </div>
+            </nav>
+        </div>
+    <!--home_Desktop-->
+    <div class="home_test container-fluid nopadding" >
+        <div class="home_image container-fluid nopadding">
+            <div class="testo">
+                <h1>Make the world<br>a better place</h1>
+            </div>
+        </div>
+        <div class="home_info">
+            <hr>
+            <h1>TITLE1</h1>
+            <img src="ressources/test.jpg" height="600px" width="1000px">
+        </div>
+    </div>
+    
+    <!--home mobile-->
+    <div class="home_testo row px-0 nopadding">
+        <div class="home_test_mobile col-12 col-sm-12 col-xs-12 nopadding">
+            <div class="testoo">
+                <h1>Make the world<br>a better place</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
