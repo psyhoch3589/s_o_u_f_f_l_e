c4cd40590164898a2c1bb96c9b939aa96606cb72
Anas Aziach <anasaziach@gmail.com>
2022-03-28 21:02:50 +0000

    update

--- Cards_admin.php
+++ Cards_admin.php
@@ -12,31 +12,31 @@
             <div class="col-md-4 col-lg-4 col-xl-4">
                 <div class="tttt input_group_Cards">
                     <label for="user_title">Title1</label>
-                    <input  type="text" name="user_title" id="user_title" >
+                    <input  type="text" name="user_title1" id="user_title" >
                 </div>
                 <div class="tttt input_group_Cards" >
                     <label for="user_description">Description1</label>
-                    <input  type="text" name="user_description" id="user_description" >
+                    <input  type="text" name="user_description1" id="user_description" >
                 </div>
             </div>
             <div class="col-md-4 col-lg-4 col-xl-4">
                 <div class="input_group_Cards">
                     <label for="user_title">Title2</label>
-                    <input  type="text" name="user_title" id="user_title" >
+                    <input  type="text" name="user_title2" id="user_title" >
                 </div>
                 <div class="input_group_Cards" >
                     <label for="user_description">Description2</label>
-                    <input  type="text" name="user_description" id="user_description" >
+                    <input  type="text" name="user_description2" id="user_description" >
                 </div>
             </div>
             <div class="col-md-4 col-lg-4 col-xl-4">
                 <div class="input_group_Cards">
                     <label for="user_title">Title3</label>
-                    <input  type="text" name="user_title" id="user_title" >
+                    <input  type="text" name="user_title3" id="user_title" >
                 </div>
                 <div class="input_group_Cards">
                     <label for="user_description">Description3</label>
-                    <input  type="text" name="user_description" id="user_description" >
+                    <input  type="text" name="user_description3" id="user_description" >
                 </div>
             </div>
         </div>
@@ -45,33 +45,30 @@
         <div class="col-xl-10 col-lg-10 col-md-10"></div>
         <div class="col-xl-2 col-lg-2 col-md-2"><button type="submit"  name="submit_cards" class="btn_submit_about">Save changes</button></div>
     </div>
-    </form>
-    
-    <?php
-    if(isset($_POST["submit_about"])){
-        try{
-            $mydatabase=new PDO("mysql:host=localhost;dbname=souffle","root","");
-        }
-        catch(exception $e){
-            Die("ERROR".$e->getMessage());
-        }
-        $picSlide=$_FILES['slide_image']['name'];
-        $tmp_dir=$_FILES['slide_image']['tmp_name'];
-        $upload_dir="uploads_admin/";
-        move_uploaded_file($tmp_dir, $upload_dir.$picSlide);
-
-        $t1=$_POST["user_title"];
-        $d1=$_POST["user_description"];
-        $t2=$_POST["user_titlee"];
-        $d2=$_POST["user_descriptionn"];
-        if($_POST["display"]==="NO") $status="no";
-        else $status="yes";
-        $sql=$mydatabase->prepare("insert into about value(?,?,?,?,?,?)");
-        $sql2="delete from about";
-        $mydatabase->exec($sql2);
-        $sql->execute(array($t1,$d1,$t2,$d2,$upload_dir.$picSlide,$status));
-        echo $status;
-        echo "<script>document.getElementById('about').classList.add('visited');</script>";
-    }
-    ?>
 </form>
+<?php
+//il faut inserer une ligne dans le tableau cards pour que le code ca marche sinon y aura pas de update
+if(isset($_POST["submit_cards"])){
+    try{
+        $mydatabase = new PDO("mysql:host=localhost;dbname=souffle","root","");
+    }catch(exception $e){
+        Die("ERROR".$e->getMessage());
+    }
+    $t1=$_POST["user_title1"];
+    $d1=$_POST["user_description1"];
+    $t2=$_POST["user_title2"];
+    $d2=$_POST["user_description2"];
+    $t3=$_POST["user_title3"];
+    $d3=$_POST["user_description3"];
+    if($t1!="") $mydatabase->exec("update cards set titre1=\"".$t1."\"");
+    if($d1!="") $mydatabase->exec("update cards set desc1=\"".$d1."\"");
+    if($t2!="") $mydatabase->exec("update cards set titre2=\"".$t2."\"");
+    if($d2!="") $mydatabase->exec("update cards set desc2=\"".$d2."\"");
+    if($t3!="") $mydatabase->exec("update cards set titre3=\"".$t3."\"");
+    if($d3!="") $mydatabase->exec("update cards set desc3=\"".$d3."\"");
+    if($_POST["display"]==="NO") $status="no";
+    else $status="yes";
+    $mydatabase->exec("update cards set status=\"".$status."\"");
+    }
+$mydatabase=null;
+?>
--- table_admin.sql
+++ table_admin.sql
@@ -1,4 +1,4 @@
 
 create table About(titre_1 varchar(150) Not NUll,descr_1 varchar(300) Not NULL,titre_2 varchar(150) Not NUll,descr_2 varchar(300) Not NULL,title_image varchar(150),status varchar(150) DEFAULT "YES");
 create table Stati(id INT Not NULL ,titre varchar(150) Not NUll,pourcentage INT NOT NULL ,statu varchar(150) DEFAULT "disabled");
-create table Cards(id INT Not NULL ,titre varchar(150) Not NUll,pourcentage INT NOT NULL ,statu varchar(150) DEFAULT "disabled");
+create table Cards(titre1 varchar(300) Not NUll,desc1 varchar(300) Not NUll,titre2 varchar(300) Not NUll,desc2 varchar(300) Not NUll,titre3 varchar(300) Not NUll,desc3 varchar(300) Not NUll,statu varchar(150) DEFAULT "enabled");
